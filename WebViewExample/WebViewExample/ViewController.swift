1db4a0ce943ef410400db728fc4df2966633cda1
JuliaVorotchenko <juliavorotchenko@gmail.com>
2020-09-17 16:30:56 +0300

    Set project

---
+++ WebViewExample/WebViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewExample
+//
+//  Created by Юлия Воротченко on 17.09.2020.
+//  Copyright © 2020 Юлия Воротченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
